ebb088537b1beda04133d726fd3455bff51baf38
Alec Brownlie <alecbrownlie@gmail.com>
2016-03-30 13:25:47 -0700

    First commit

--- BookSmart/AppDelegate.swift
+++ BookSmart/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // Initialize Parse.
+        Parse.setApplicationId("pGQQu6YGEdBgjX0QrbuJvu7BDZ9rp094yyFmlx6V",
+            clientKey: "LNoNKtql2LPKKzt1rU8khbcPFANVsdyG9DLRtUz5")
         return true
     }
 
--- BookSmart/ViewController.swift
+++ BookSmart/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UsernameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBAction func LogInButton(sender: AnyObject)
+    {
+        LogIn()
+    }
+    @IBAction func SignUpButton(sender: AnyObject)
+    {
+        SignUp()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func LogIn()
+    {
+        var user = PFUser()
+        user.username = UsernameTextField.text
+        user.password = PasswordTextField.text
+        user.email = EmailTextField.text
+        
+        PFUser.logInWithUsernameInBackground(UsernameTextField.text!, password: PasswordTextField.text!, block: {
+            (User : PFUser?, Error: NSError?) -> Void in
+            
+            if Error == nil
+            {
+                dispatch_async(dispatch_get_main_queue()){
+                    var Storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    var MainVC : UIViewController = Storyboard.instantiateViewControllerWithIdentifier("MainVC")
+                    self.presentViewController(MainVC, animated: true, completion: nil)
+                }
+            }
+            else
+            {
+                NSLog("Wrong Password")
+            }
+            
+        })
+    }
 
+    func SignUp()
+    {
+        var user = PFUser()
+        user.username = UsernameTextField.text
+        user.password = PasswordTextField.text
+        user.email = EmailTextField.text
+        
+        user.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if error == nil{
+                // Hooray! Let them use app now
+            }
+            else
+            {
+                // Examine error object and inform user
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
